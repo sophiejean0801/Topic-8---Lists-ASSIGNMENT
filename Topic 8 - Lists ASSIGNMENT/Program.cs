f4c5cc181331bdcaa8e0a711eb50bfa27a31657e
Sophie Jean <363449299@GoTVDSB.ca>
2025-04-10 14:03:56 -0400

    finished

--- Topic 8 - Lists ASSIGNMENT/Program.cs
+++ Topic 8 - Lists ASSIGNMENT/Program.cs
@@ -9,30 +9,85 @@
             Random number = new Random();
             List<string> colour = new List<string>();
             int inNumber, listSize, maxNumber, minNumber;
+            bool done = false;
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Enter a colour:");
-                string input = Console.ReadLine();
-                colour.Add(input);
-            }
-            Console.WriteLine("The colours you entered are:");
-            Console.Write(string.Join(", ", colour));
-            Console.ReadLine();
-            Console.WriteLine("The random one is: ");
-            Console.WriteLine(colour[number.Next(colour.Count)]);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Enter a colour:");
+            //    string input = Console.ReadLine();
+            //    colour.Add(input);
+            //}
+            //Console.WriteLine("The colours you entered are:");
+            //Console.Write(string.Join(", ", colour));
+            //Console.ReadLine();
+            //Console.WriteLine("The random one is: ");
+            //Console.WriteLine(colour[number.Next(colour.Count)]);
 
             // Assignment 2
 
             List<int> numbers = new List<int>();
-            Console.WriteLine("Enter a number: ");
-            inNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the size of the list:");
             listSize = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the maximum number:");
-            maxNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the minimum number:");
             minNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the maximum number:");
+            maxNumber = Convert.ToInt32(Console.ReadLine());
+            while (minNumber >= maxNumber)
+            {
+                Console.WriteLine("The minimum number must be less than the maximum number. Please enter the minimum number again:");
+                minNumber = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < listSize; i++)
+            {
+                numbers.Add(number.Next(minNumber, maxNumber));
+            }
+            Console.WriteLine("The numbers in the list are:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("From the numbers shown, which one do you choose? I will count how many times it shows up in the list.");
+            inNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == inNumber)
+                {
+                    Console.WriteLine("The number " + inNumber + " shows up " + numbers.Count(x => x == inNumber) + " times.");
+                }
+                
+            }
+            Console.WriteLine("Pick a number from the list to turn into 0");
+            inNumber = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == inNumber)
+                {
+                    numbers[i] = 0;
+                }
+            }
+            Console.WriteLine("The numbers in the list are:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine(numbers.Count(x => x == 0) + " numbers were changed to 0.");
+            Console.WriteLine("The numbers in the list are:");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("Lets make a new list");
+            List<int> numbers2 = new List<int>();
+            for (int i = 0; i < listSize; i++)
+            {
+                numbers.Add(number.Next(minNumber, maxNumber));
+            }
+
+
+
+
+
+
 
 
 
